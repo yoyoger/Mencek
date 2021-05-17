a4df3ada76dd39a13e394905967a7b1d5fcaee48
yoyoger <prostudovat@gmail.com>
2021-05-18 01:27:55 +0900

    layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def base_title
+    "らーめんの里"
+  end
+
+  def full_title(page_title='')
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
