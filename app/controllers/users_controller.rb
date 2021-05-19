6f5387c21dd1401c4412086df7b5d3b56168659d
yoyoger <prostudovat@gmail.com>
2021-05-19 22:14:19 +0900

    login & logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "アカウントの作成に成功しました。"
       redirect_to @user
     else
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to: 'static_pages#home'
   get '/user_index', to: 'users#index'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   resources :users, only:[:show,:edit,:update,:destroy]
 end
