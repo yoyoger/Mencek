bb6c5a28f93d2825c7409fff4ea5814dce66f0e9
yoyoger <prostudovat@gmail.com>
2021-05-24 22:46:02 +0900

    remove bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,15 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+
+  def greeting
+    hour = Time.current.hour
+    if (5 <= hour) && (hour <=10)
+      "おはようございます"
+    elsif (11 <= hour) && (hour <= 17)
+      "こんにちは"
+    else
+      "こんばんは"
+    end
+  end
 end
